870e66040125ffe7a4c0fe0391abc13f62d59a79
呆鹅 <changcenghui@changzenghui.local>
2024-03-02 10:21:42 +0800

    2

--- app/app.go
+++ app/app.go
@@ -1,13 +1,23 @@
 package app
 
-import "github.com/1234567909/githupaa/mysql"
+import (
+	"github.com/1234567909/githupaa/config"
+	"github.com/1234567909/githupaa/mysql"
+)
 
-func Init(apps ...string) error {
+func Init(serviceName string, apps ...string) error {
 	var err error
+	err = config.GetClient()
+	if err != nil {
+		return err
+	}
 	for _, val := range apps {
 		switch val {
-		case val:
-			err = mysql.InitMysql()
+		case "mysql":
+			err = mysql.InitMysql(serviceName)
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
 	return err
